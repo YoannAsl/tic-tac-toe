2848ff76a6f545cec17ba96e5789b9a4ec797762
YoannAsl <47599801+YoannAsl@users.noreply.github.com>
2021-10-30 00:45:15 +0200

    feat: add minimax algorithm (is bugged: fills the whole board in one move)

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -15,6 +15,7 @@ const WINNING_CONFIGURATIONS = [
 function calculateWinner(cells: any[]) {
     for (const config of WINNING_CONFIGURATIONS) {
         const [a, b, c] = config;
+        // If a player has a winning config
         if (cells[a] && cells[a] === cells[b] && cells[a] === cells[c])
             return cells[a];
     }
@@ -30,9 +31,23 @@ function Game() {
         const currentCells = [...cells];
 
         if (currentCells[cellIndex] === null) {
-            currentCells[cellIndex] = isHumanPlaying ? 'X' : 'O';
-            setCells(currentCells);
+            let newIsHumanPlaying = true;
+
+            currentCells[cellIndex] = newIsHumanPlaying ? 'X' : 'O';
+            newIsHumanPlaying = !newIsHumanPlaying;
             setIsHumanPlaying(!isHumanPlaying);
+
+            const bestCell = calculateBestCell(
+                currentCells,
+                isHumanPlaying ? 'X' : 'O'
+            );
+
+            if (bestCell !== -1) {
+                currentCells[bestCell] = newIsHumanPlaying ? 'X' : 'O';
+
+                setCells(currentCells);
+                setIsHumanPlaying(!isHumanPlaying);
+            }
         }
     }
 
@@ -51,6 +66,46 @@ function Game() {
             <button onClick={resetGame}>New game</button>
         </div>
     );
+}
+
+function calculateBestCell(board: any[], player: string) {
+    const opponent = player === 'X' ? 'O' : 'X';
+
+    function minimax(board: any[], isMaximizing: boolean, depth = 0) {
+        const winner = calculateWinner(board);
+
+        if (winner === player) return { cell: -1, score: 1 };
+        if (winner === opponent) return { cell: -1, score: -1 };
+        if (board.every((cell) => cell !== null)) return { cell: -1, score: 0 };
+
+        const bestCell = {
+            cell: -1,
+            score: isMaximizing ? -10 : 10,
+        };
+
+        for (let i = 0; i < board.length; i++) {
+            // If the cell is not filled
+            if (!board[i]) {
+                board[i] = isMaximizing ? player : opponent;
+                const score = minimax(board, !isMaximizing).score;
+                // board[i] = null
+
+                if (isMaximizing) {
+                    if (score > bestCell.score) {
+                        bestCell.score = score;
+                        bestCell.cell = i;
+                    }
+                } else {
+                    if (score < bestCell.score) {
+                        bestCell.score = score;
+                        bestCell.cell = i;
+                    }
+                }
+            }
+        }
+        return bestCell;
+    }
+    return minimax(board, true).cell;
 }
 
 export default Game;
