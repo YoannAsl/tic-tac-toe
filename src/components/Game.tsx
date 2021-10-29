a28cc88c1ee864c6f0850236f15f6816b6a39ec7
YoannAsl <47599801+YoannAsl@users.noreply.github.com>
2021-10-29 21:02:08 +0200

    feat: add winner detection

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,9 +1,30 @@
 import React, { useState } from 'react';
 import Board from './Board';
 
+const WINNING_CONFIGURATIONS = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [6, 4, 2],
+];
+
+function calculateWinner(cells: any[]) {
+    for (const config of WINNING_CONFIGURATIONS) {
+        const [a, b, c] = config;
+        if (cells[a] && cells[a] === cells[b] && cells[a] === cells[c])
+            return cells[a];
+    }
+    return null;
+}
+
 function Game() {
     const [cells, setCells] = useState(Array(9).fill(null));
     const [isHumanPlaying, setIsHumanPlaying] = useState(true);
+    const winner = calculateWinner(cells);
 
     function handleCellClick(i: number) {
         const currentCells = [...cells];
@@ -15,6 +36,7 @@ function Game() {
 
     return (
         <div>
+            {winner && `The winner is ${winner}`}
             <Board
                 cells={cells}
                 onCellClick={(i: number) => handleCellClick(i)}
