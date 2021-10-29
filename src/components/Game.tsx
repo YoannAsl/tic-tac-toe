b200a90846b1c7294109c7a42db96f076e17fd56
YoannAsl <47599801+YoannAsl@users.noreply.github.com>
2021-10-29 20:37:08 +0200

    feat: add turn alternation

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Board from './Board';
 
 function Game() {
+    const [cells, setCells] = useState(Array(9).fill(null));
+    const [isHumanPlaying, setIsHumanPlaying] = useState(true);
+
+    function handleCellClick(i: number) {
+        const currentCells = [...cells];
+        currentCells[i] = isHumanPlaying ? 'X' : 'O';
+
+        setCells(currentCells);
+        setIsHumanPlaying(!isHumanPlaying);
+    }
+
     return (
         <div>
             <Board
