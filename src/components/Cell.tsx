11c523da9812488b142283514d4c855a75ec05a6
YoannAsl <47599801+YoannAsl@users.noreply.github.com>
2021-11-01 20:42:33 +0100

    feat: add icons for O and X

--- src/components/Cell.tsx
+++ src/components/Cell.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
+import circleIcon from '../assets/images/circleIcon.svg';
+import crossIcon from '../assets/images/crossIcon.svg';
 
 interface CellProps {
     value: string | null;
@@ -7,11 +9,23 @@ interface CellProps {
 }
 
 function Cell({ value, onClick }: CellProps) {
-    return <Container onClick={onClick}>{value}</Container>;
+    return (
+        <Container onClick={onClick}>
+            {value === 'X' && <img src={crossIcon} alt='cross icon' />}
+            {value === 'O' && <img src={circleIcon} alt='circle icon' />}
+        </Container>
+    );
 }
 
 const Container = styled.div`
     border: 1px solid black;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    img {
+        width: 150px;
+        height: 150px;
+    }
 `;
 
 export default Cell;
